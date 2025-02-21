76843bbfd25e081c34ed0e4fdd89b3ae72852226
NielPatelSE12 <NielPatel1212@outlook.com>
2025-02-21 11:11:37 -0500

    Initial Commit

---
+++ Bluetooth Final Final/Bluetooth_Final_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_Final_FinalApp.swift
+//  Bluetooth Final Final
+//
+//  Created by Niel Patel on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Bluetooth_Final_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bluetooth Final FinalTests/Bluetooth_Final_FinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bluetooth_Final_FinalTests.swift
+//  Bluetooth Final FinalTests
+//
+//  Created by Niel Patel on 2/21/25.
+//
+
+import Testing
+@testable import Bluetooth_Final_Final
+
+struct Bluetooth_Final_FinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
